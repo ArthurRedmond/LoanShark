bd3724cff8335d1a7d16c6df5a8fd4b6f3cf56b3
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-10-15 11:17:33 -0400

    Updated table

--- js/site.js
+++ js/site.js
@@ -9,7 +9,7 @@ function getValues() {
 
     if (!Number.isNaN(loanAmount) && !Number.isNaN(numberOfPayments) && !Number.isNaN(interestRate)) {
         let loanArray = calculateValues(loanAmount, numberOfPayments, interestRate);
-        displayResults(loanAmount, loanArray);
+        displayResults(loanArray);
     } else {
         alert("You must enter valid numbers");
     }
@@ -24,39 +24,55 @@ function calculateValues(loanAmount, numberOfPayments, interestRate) {
         returnArray.push(i);
 
         let monthlyPayment = (loanAmount * ((interestRate / 1200) / (1 - Math.pow(1 + (interestRate / 1200), (numberOfPayments * -1)))));
-        // (loanAmount * ((interestRate/1200) / (1-Math.pow(1+(interestRate/1200),(numberOfPayments * -1)))))
-        returnArray.push(monthlyPayment.toFixed(2));
+        returnArray.push(monthlyPayment);
 
         let interestPaid = remainingBalance * (interestRate / 1200);
         let principalPaid = monthlyPayment - interestPaid
 
-        returnArray.push(principalPaid.toFixed(2));
+        returnArray.push(principalPaid);
 
-        returnArray.push(interestPaid.toFixed(2));
+        returnArray.push(interestPaid);
 
         totalInterestPaid += interestPaid;
-        returnArray.push(totalInterestPaid.toFixed(2));
+        returnArray.push(totalInterestPaid);
 
         remainingBalance -= principalPaid;
-        returnArray.push(remainingBalance.toFixed(2));
+        returnArray.push(remainingBalance);
     }
 
+    document.getElementById("monthlyPayment").classList.add("output-payment");
+    document.getElementById("monthlyPayment").innerHTML = `$${returnArray[1].toFixed(2)}`;
+    document.getElementById("totalPrincipal").innerHTML = `$${loanAmount}`;
+    document.getElementById("totalInterest").innerHTML = `$${totalInterestPaid.toFixed(2)}`;
     let totalCost = totalInterestPaid + loanAmount;
-    document.getElementById("totalCost").innerHTML = `${totalCost.toFixed(2)}`;
+    document.getElementById("totalCost").innerHTML = `$${totalCost.toFixed(2)}`;
 
     return returnArray;
 }
 
-function displayResults(loanAmount, loanArray) {
+function displayResults(loanArray) {
+    //Get the table body element
     let tableBody = document.getElementById("results");
 
-    let tableRow = document.getElementById("outputTemplate");
+    //Get the template row
+    let templateRow = document.getElementById("outputTemplate");
 
+    //clear the table
     tableBody.innerHTML = "";
-    //monthlyPayment
-    //totalPrincipal
-    //totalInterest
-    //totalCost
 
+    for (let index = 0; index < loanArray.length; index += 6) {
 
+        let tableRow = document.importNode(templateRow.content, true);
+
+        let rowCols = tableRow.querySelectorAll("td");
+
+        rowCols[0].textContent = loanArray[index];
+        rowCols[1].textContent = loanArray[index + 1].toFixed(2);
+        rowCols[2].textContent = loanArray[index + 2].toFixed(2);
+        rowCols[3].textContent = loanArray[index + 3].toFixed(2);
+        rowCols[4].textContent = loanArray[index + 4].toFixed(2);
+        rowCols[5].textContent = loanArray[index + 5].toFixed(2);
+
+        tableBody.appendChild(tableRow);
+    }
 }
